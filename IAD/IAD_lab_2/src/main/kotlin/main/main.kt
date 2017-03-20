ccdb812567e99ec6c20ad8ab3603ef3c5988d610
Tiofx <ioracle2718@gmail.com>
2017-03-20 22:34:15 +0300

    population parser was created

--- IAD/IAD_lab_2/src/main/kotlin/main/main.kt
+++ IAD/IAD_lab_2/src/main/kotlin/main/main.kt
@@ -1,19 +1,18 @@
 package main
 
-import util.Area
-import util.batch
-import util.Gdp
-import util.getFromResources
+import util.*
 import java.io.File
 
 fun main(args: Array<String>) {
     val set = "countries/africa.txt".getFromResources().parseCountryList()
     val areaFile = "area.txt".getFromResources()
     val gdp = "gdp.txt".getFromResources()
+    val population = "population.txt".getFromResources()
 
     println(set)
     println(areaFile.parseArea())
     println(gdp.parseGdp())
+    println(population.parsePopulation())
 }
 
 fun File.parseCountryList() = this.readLines()
@@ -43,4 +42,18 @@ fun File.parseGdp() = this.readLines()
         }
         .toList()
 
-fun File.parsePopulation() = 1
+fun File.parsePopulation() = this.readLines()
+        .map(String::trim)
+        .map { it.replace(",", ".") }
+        .filter(String::isNotEmpty)
+        .asSequence()
+        .batch(4)
+        .map {
+            Population(
+                    it[0].toInt(),
+                    it[1],
+                    it[2].replace(" ", "").toInt(),
+                    it[3].replace("%", "").toDouble()
+            )
+        }
+        .toList()
