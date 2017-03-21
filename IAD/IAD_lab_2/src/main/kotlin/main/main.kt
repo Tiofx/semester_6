a80ef331523f5c7ea89666b40c20ffd544aafcfb
Tiofx <ioracle2718@gmail.com>
2017-03-21 12:27:47 +0300

    getColumn function was implemented

--- IAD/IAD_lab_2/src/main/kotlin/main/main.kt
+++ IAD/IAD_lab_2/src/main/kotlin/main/main.kt
@@ -6,12 +6,28 @@ import util.getShortCountryName
 import util.parseCountrySet
 
 fun main(args: Array<String>) {
+    val europe = "countries/europe.txt".getFromResources().parseCountrySet()
+    val america = "countries/america.txt".getFromResources().parseCountrySet()
+    val asia = "countries/asia.txt".getFromResources().parseCountrySet()
     val africa = "countries/africa.txt".getFromResources().parseCountrySet()
 
     val unionInformation = getUnionInformation()
 
-    println(unionInformation.getInfoAbout(africa).size)
+    val europeInfo = unionInformation.getInfoAbout(europe)
+    val americaInfo = unionInformation.getInfoAbout(america)
+    val asiaInfo = unionInformation.getInfoAbout(asia)
+    val africaInfo = unionInformation.getInfoAbout(africa)
 }
+
+fun Set<CountryInformation>.getColumn(number: Int) = this.map {
+    when (number) {
+        0 -> it.name
+        1 -> it.area
+        2 -> it.population
+        3 -> it.gdp
+        else -> throw java.security.InvalidParameterException()
+    }
+}.toSet()
 
 
 fun Set<CountryInformation>.getInfoAbout(partOfWorld: Set<String>) = this.filter {
