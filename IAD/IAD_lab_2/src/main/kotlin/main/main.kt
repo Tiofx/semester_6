087eadd7ccceed71872fc74d46b89c4a7988af24
Tiofx <ioracle2718@gmail.com>
2017-03-21 11:40:49 +0300

    union all data function was implemented

--- IAD/IAD_lab_2/src/main/kotlin/main/main.kt
+++ IAD/IAD_lab_2/src/main/kotlin/main/main.kt
@@ -13,7 +13,35 @@ fun main(args: Array<String>) {
     val gdp = gdpFile.parseGdp()
     val population = populationFile.parsePopulation()
 
+    val union = union(area, population, gdp)
+
+    println(union)
 }
+
+fun union(area: List<Area>, population: List<Population>, gdp: List<Gdp>): Set<CountryInformation> {
+    val countrySet = area.map {
+        val countryInformation = CountryInformation(it.country.getShortCountryName())
+        countryInformation.area = it.area
+        countryInformation
+    }.toSet()
+
+    population.forEach {
+        val countryName = it.country
+        countrySet.find { it.name == countryName.getShortCountryName() }?.population = it.populationAbsolute
+    }
+
+    gdp.forEach {
+        val countryName = it.country
+        countrySet.find { it.name == countryName.getShortCountryName() }?.gdp = it.gdp
+    }
+
+    val countryWithFullInfo = countrySet.filterNot {
+        it.area == -1.0 || it.population == -1 || it.gdp == -1.0
+    }.toSet()
+
+    return countryWithFullInfo
+}
+
 
 fun String.getShortCountryName() = this.substringBefore('(').trim()
 
