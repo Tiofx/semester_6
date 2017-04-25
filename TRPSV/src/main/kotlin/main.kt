4514c5d212473aba910aadfb0aeec16c7c609652
Tiofx <ioracle2718@gmail.com>
2017-04-25 10:02:38 +0300

    refactoring

--- TRPSV/src/main/kotlin/main.kt
+++ TRPSV/src/main/kotlin/main.kt
@@ -1,24 +1,34 @@
-import mpi.MPI
+import task1.plotTask1Result
+import task1.testTask1
 import task2.test.*
 import task3.plotTask3Result
+import task3.rank
 import task3.testTask3
 import visualization.allEdgeProbabilityPlot
-import java.io.File
-import java.util.*
 import kotlin.system.measureNanoTime
 
 //
 val elementNumber = 8 * 1e3.toInt()
 val iterationNumber = 88
 
+fun task1(args: Array<String>) {
+    val rank = rank(args)
+
+    var task1 = mutableListOf<Pair<Int, ParallelAndSequentialTime>>()
+    println(measureNanoTime { task1 = testTask1(args) } / 1e9)
+
+    if (rank == 0) {
+        plotTask1Result(task1)
+    }
+}
+
 fun task2(args: Array<String>) {
-    MPI.Init(args)
-    val rank = MPI.COMM_WORLD.Rank()
-    MPI.Finalize()
+    val rank = rank(args)
 
     var makeTest = ResultSet(listOf())
     val measureNanoTime = measureNanoTime { makeTest = makeTest(args, testSet("temp.json")) }
     println(measureNanoTime / 1e9)
+
     if (rank == 0) {
         allEdgeProbabilityPlot(makeTest)
         println(makeTest.toJson().toJsonString(true))
@@ -26,9 +36,7 @@ fun task2(args: Array<String>) {
 }
 
 fun task3(args: Array<String>) {
-    MPI.Init(args)
-    val rank = MPI.COMM_WORLD.Rank()
-    MPI.Finalize()
+    val rank = rank(args)
 
     var testTask3Result = mutableListOf<Triple<Int, Int, ParallelAndSequentialTime>>()
     println(measureNanoTime { testTask3Result = testTask3(args) } / 1e9)
@@ -39,27 +47,9 @@ fun task3(args: Array<String>) {
 }
 
 fun main(args: Array<String>) {
-    task3(args)
-
-//    val parallelTime = task1.parallelTask1(args)
-
-
-//    if (MPI.COMM_WORLD.Rank() == 0) {
-//        println(parallelTime
-//                .map { it / 1e6 }
-//                .map(Any::toString)
-//                .reduce { acc, s -> "$acc\n$s" })
-//
-//        println("""
-//            |-------------------------------------------------------------
-//            | количество элементов: $kotlin.getElementNumber
-//            | количество итераций: $kotlin.getIterationNumber
-//            | затраченное время на последовательную реализацию: ${sequentialTask1(kotlin.getElementNumber, kotlin.getIterationNumber) / 1e6} мс
-//            | затраченное время на параллельную реализацию:     ${parallelTime.drop((kotlin.getIterationNumber * 0.1).toInt()).average() / 1e6} мс
-//            |-------------------------------------------------------------
-//    """.trimMargin())
-
-//    }
+    task1(args)
+//    task2(args)
+//    task3(args)
 }
 
 const val filename: String = "array.txt"
