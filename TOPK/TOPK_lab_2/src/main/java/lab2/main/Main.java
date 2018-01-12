9da4f9e3b6f6adc6b7878b226147f4496051cefb
Tiofx <ioracle2718@gmail.com>
2018-01-12 23:00:23 +0300

    5) Introduce Foreign Method

--- TOPK/TOPK_lab_2/src/main/java/lab2/main/Main.java
+++ TOPK/TOPK_lab_2/src/main/java/lab2/main/Main.java
@@ -4,25 +4,31 @@ import lab2.util.AutomationFactory;
 import lab2.util.variantNative.CodesAnalyser;
 import lab2.util.variantNative.Constants;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
+
 
 public class Main {
+
+    private static void sendLines(BufferedReader from, Automation to) {
+        from.lines().forEach(to::sendLine);
+    }
+
+    private static void fromFileByLine(Automation automation, String filename) throws FileNotFoundException {
+        new BufferedReader(new FileReader(new File(filename)))
+                .lines()
+                .forEach(automation::sendLine);
+        //                .forEach(s -> s.chars()
+//                        .mapToObj(i -> (char) i)
+//                        .forEachOrdered(automation::sendCharacter)
+//                );
+    }
 
     public static void main(String[] args) throws FileNotFoundException {
         String filename = "test_lab2.txt";
         Automation automation = AutomationFactory.createAutomation(Constants.myVariant);
         CodesAnalyser analyser = new CodesAnalyser();
 
-        new BufferedReader(new FileReader(new File(filename)))
-                .lines()
-                .forEach(automation::sendLine);
-//                .forEach(s -> s.chars()
-//                        .mapToObj(i -> (char) i)
-//                        .forEachOrdered(automation::sendCharacter)
-//                );
+        fromFileByLine(automation, filename);
 
         String result =
                 automation.getLog().stream()
