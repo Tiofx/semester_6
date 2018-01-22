f3d89905b5ed9eab668d40abe7df9eed2243351c
Tiofx <ioracle2718@gmail.com>
2018-01-22 11:26:41 +0300

    [add] Adapter of RegEX to Automation

---
+++ TOPK/TOPK_lab_1/src/main/java/lab1/main/Adapter.kt
@@ -0,0 +1,26 @@
+package lab1.main
+
+import lab1.main.FiniteStateAutomaton.Result
+
+class Adapter(regexString: String) : FiniteStateAutomatonInterface {
+    private var matcher = getMatcherByRegex(regexString)
+
+    fun changeRegex(regexString: String) {
+        matcher = getMatcherByRegex(regexString)
+    }
+
+    private fun getMatcherByRegex(regexString: String) =
+            regexString.toPattern().matcher("")
+
+
+    override fun check(input: String?): FiniteStateAutomaton.Result =
+            matcher.run {
+                reset(input)
+
+                when {
+                    matches() -> Result.RIGHT
+                    hitEnd() -> Result.ONGOING
+                    else -> Result.WRONG
+                }
+            }
+}
